ca99e0c0f0454c5e678c576ec1d7bbd069b9f96c
Zpecena <57196930+Zpecena@users.noreply.github.com>
2020-11-22 19:10:10 +0100

    zabudnute validacie pri uprave

--- Semestralka/App/Views/Checkpoint2/upravit.view.php
+++ Semestralka/App/Views/Checkpoint2/upravit.view.php
@@ -3,7 +3,7 @@
     <input type="hidden" value="<?= @$_GET['id']?>" name="id">
     <div class="form-group">
         <label for="meno">Meno</label>
-        <input type="text" class="form-control" id="meno" name="meno" placeholder="Niekto Nejaky" value="<?= @$data->getMeno()?>" >
+        <input type="text" class="form-control" id="meno" name="meno" placeholder="Niekto Nejaky" value="<?= @$data->getMeno()?>" required maxlength="25" minlength="5">
     </div>
     <div class="form-group">
         <label for="pohlavie">Pohlavie</label>
@@ -15,7 +15,7 @@
     </div>
     <div class="form-group">
         <label for="popis">Kratky popis</label>
-        <textarea class="form-control" id="popis" rows="3" name="popis" ><?= @$data->getPopis()?></textarea>
+        <textarea class="form-control" id="popis" rows="3" name="popis" required minlength="20" maxlength="150"><?= @$data->getPopis()?></textarea>
     </div>
     <div>
         <input type="submit" value="odoslat">
